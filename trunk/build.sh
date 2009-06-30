4f90428bf214301e79b0a9f952506de1fe408052
markus.gylling <markus.gylling@f2257d64-4077-11de-af2b-f15df6ab9c44>
2009-06-30 12:18:02 +0000

    Share project "daisy-online" into "https://daisy-online-delivery.googlecode.com/svn"

---
+++ trunk/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+if [ ! -d ./build/ ]
+then
+    mkdir "build"
+fi
+
+cp=./lib/calabash.jar:./lib/saxon9-s9api.jar:./lib/jing.jar
+java -cp $cp com.xmlcalabash.drivers.Main --with-param section.autolabel=1 --with-param section.label.includes.component.label=1 --with-param html.stylesheet=do.css build.xpl 
+cp ./util/docbook/html-css/docbook.css ./build/do.css
